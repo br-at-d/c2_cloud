89b07dd9924e364a1f51bcb4ab067702e1eb76ef
br-at-d <35181705+br-at-d@users.noreply.github.com>
2019-08-30 17:12:12 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -21,5 +21,18 @@ systemctl enable hak5c2.service
 echo "What is your site's URL (without http(s)://)?"
 read HOSTNAME
 sed -i "s/YOUR.URL.HERE/${HOSTNAME}/g" /usr/bin/c2_start.sh
+
+echo "What is your database name? (enter "c2" for the default)?"
+read DBASE
+sed -i "s/YOUR.DB.NAME/${DBASE}/g" /usr/bin/c2_start.sh
+
+echo "What is your listening port? (enter "8080" for the default)?"
+read LPORT
+sed -i "s/YOUR.LPORT.NUMBER/${LPORT}/g" /usr/bin/c2_start.sh
+
+echo "What is your ssh port? (enter "2022" for the default)?"
+read SSHPORT
+sed -i "s/YOUR.SSHPORT.NUMBER/${SSHPORT}/g" /usr/bin/c2_start.sh
+
 Echo "All set, run the c2_start.sh to get the initial setup token. Going forward you can use the "service hak5c2 start|stop|status" commands.
 
