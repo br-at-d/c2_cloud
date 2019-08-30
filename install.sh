53a686334cc53f68189a99c39a7712726dcb2176
br-at-d <35181705+br-at-d@users.noreply.github.com>
2019-08-30 17:16:35 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ mkdir /c2 && cd /c2
 wget https://c2.hak5.org/download/community && mv commmunity c2_community.zip
 unzip c2_community
 #Download GoVanguard C2 Files
-git clone https://github.com/GoVanguard/c2_cloud.git
+git clone https://github.com/br-at-d/c2_cloud.git
 chown -R root c2_cloud
 cp c2_cloud/usr/bin/c2_start.sh /usr/bin/c2_start.sh
 cp c2_cloud/etc/systemd/system/hak5c2.service /etc/systemd/system/hak5c2.service
