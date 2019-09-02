04c008b3ff17c033582fa95a7e414c79f84da986
br-at-d <35181705+br-at-d@users.noreply.github.com>
2019-09-02 20:25:41 -0300

    Create c2_start.sh

---
+++ c2_start.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+HOSTNAME=YOUR.URL.HERE
+DBASE=YOUR.DB.NAME
+LPORT=YOUR.LPORT.NUMBER
+SSHPORT=YOUR.SSHPORT.NUMBER
+
+# Root Priv Check
+if [ "$(whoami)" != "root" ]; then
+                echo "Sorry, you are not root."
+                        exit 1
+                fi
+
+#Ensure no existing c2 instances are running
+
+killall c2_community-linux-arm7
+
+#Start Hak5 C2 Community Edition
+
+cd /c2/
+./c2_community-linux-arm7 -db $DBASE -https -hostname $HOSTNAME -listenport $LPORT -sshport $SSHPORT
